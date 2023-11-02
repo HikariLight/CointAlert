b181399d9b02735f5bd1c6327fd6d3d1f600e567
Dhia M <merzougui.dhia@gmail.com>
2023-11-02 16:00:24 +0100

    Misc style changes to AlertBox and homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react"
 import AlertBox from "./components/AlertBox"
 import Navbar from "./components/Navbar"
+import CustomButton from "./components/CustomButton"
 
 const App = () => {
-  
-  const apiUrl = 'http://127.0.0.1:8000/getAlerts'
-  const [alerts, setAlerts] = useState([])
+    const apiUrl = "http://127.0.0.1:8000/getAlerts"
+    const [alerts, setAlerts] = useState([])
 
-  useEffect(() => {
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => { setAlerts(JSON.parse(data))})
-      .catch((error) => {
-        console.error('Error fetching data:', error)
-      })
-  }, [])
+    useEffect(() => {
+        fetch(apiUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                setAlerts(JSON.parse(data))
+            })
+            .catch((error) => {
+                console.error("Error fetching data:", error)
+            })
+    }, [])
 
     const getAlerts = async () => {
         fetch(apiUrl)
@@ -33,18 +35,21 @@ const App = () => {
         <div className="h-screen w-3/4 mx-auto">
             <Navbar />
 
-      <div className="flex flex-col border border-purple-800 rounded m-2 p-2">
-        <h1 className="text-2xl my-2 text-purple-800 text-center">CointAlert</h1>
-        <button className="rounded bg-purple-800 text-white p-4" onClick={() => getAlerts()}>Get Alerts</button>
-      </div>
+            <div className="flex flex-col border border-purple-800 rounded m-2 p-2">
+                <h1 className="text-2xl my-2 text-purple-800 text-center">
+                    CointAlert
+                </h1>
 
-      <div className="grid grid-cols-3 gap-3">
-        {alerts.map((alert, index) => (
-          <AlertBox alert={alert} key={index} />
-        ))}
-      </div>
-    </div>
-  )
+                <CustomButton content="Get Alerts" func={getAlerts} />
+            </div>
+
+            <div className="grid grid-cols-3 gap-3">
+                {alerts.map((alert, index) => (
+                    <AlertBox alert={alert} key={index} />
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default App
--- frontend/src/components/AlertBox.jsx
+++ frontend/src/components/AlertBox.jsx
@@ -1,7 +1,9 @@
-const AlertBox = ({alert}) => {
-    return(
+const AlertBox = ({ alert }) => {
+    return (
         <div className="border rounded border-purple-800 p-2">
-            <h1 className="text-xl text-purple-800">Alert: {alert.name}</h1>
+            <h1 className="text-xl text-purple-800">
+                Alert: {alert.alert_name}
+            </h1>
             <h3>Occurred on: {alert.date}</h3>
             <h3>Alert limit: {alert.limit}</h3>
             <h3>Price reached: {alert.price}</h3>
