43e6a04ab8ed14ff2071f3dacec33c5f308954e9
Dhia M <merzougui.dhia@gmail.com>
2023-11-06 10:16:19 +0100

    Improved exception handling

--- backend/DB.py
+++ backend/DB.py
@@ -3,6 +3,10 @@ def get_alert_definitions(supabase_client):
         response = supabase_client.table('alert_definitions').select("*").execute()
     except Exception as e:
         print(" > [Startup] Alert fetching alert definitions: ", e)
+        return {
+            "status": "fail",
+            "reason": str(e)
+        }
 
     return response.data
 
@@ -11,5 +15,9 @@ def get_alerts(supabase_client):
         response = supabase_client.table('alerts').select("*").execute()
     except Exception as e:
         print(" > [Startup] Alert fetching alert definitions: ", e)
+        return {
+            "status": "fail",
+            "reason": str(e)
+        }
 
     return response.data
